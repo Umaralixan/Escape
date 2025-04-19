b2ccc7813af07fd18e0c2181c1c681fe8186bbdf
Umaralixan <umaralixtreme@gmail.com>
2025-04-19 14:29:47 +0500

    first commit

---
+++ sytle.js
@@ -0,0 +1,128 @@
+// if 1
+// let son = 5;
+// if (son > 0) {
+//   son = son + 1;
+// }
+// console.log(son);
+
+// if 2
+// let son = parent(prompt);
+// son = son > 0 ? son + 1 : son - 2;
+// console.log(son)
+
+// if 3
+// let son = +prompt("Butun son kiriting");
+// if (son > 0) {
+//   son = son + 1;
+// } else if (son < 0) {
+//   son = son - 2;
+// } else {
+//   son = 10;
+// }
+// console.log(son);
+
+// if 4
+// let a = prompt("a ni kiriting");
+// let b = prompt("b ni kiriting");
+// let c = prompt("c ni kiriting");
+
+// let son = 0;
+
+// if (a > 0) {
+//   son++;
+// }
+// if (b > 0) {
+//   son++;
+// }
+// if (c > 0) {
+//   son++;
+// }
+// console.log(son);
+
+// if 5
+// let a = parseInt(prompt("a ni ikiriting"));
+// let b = parseInt(prompt("b ni ikiriting"));
+// let c = parseInt(prompt("c ni ikiriting"));
+// let musbat = 0;
+// let manfiy = 0;
+// let son = 0;
+
+// if (son(a)) {
+//   if (a > 0) {
+//     musbat++;
+//   } else if (a < 0) {
+//     manfiy++;
+//   }
+// }
+// if (son(b)) {
+//   if (b > 0) {
+//     musbat++;
+//   } else if (b < 0) {
+//     manfiy++;
+//   }
+// }
+// if (son(c)) {
+//   if (c > 0) {
+//     musbat++;
+//   } else if (c < 0) {
+//     manfiy++;
+//   }
+// }
+// console.log(musbat++);
+// console.log(manfiy++);
+
+// if 6
+// let a = parseInt(prompt("a ni ikiriting"));
+// let b = parseInt(prompt("b ni ikiriting"));
+// let son;
+// if (a > b) {
+//   son = a;
+// } else if (b > a) {
+//   son = b;
+// } else {
+//   a = b;
+// }
+// console.log(son)
+
+// if 7
+// let a = parseInt(prompt("a ni ikiriting"));
+// let b = parseInt(prompt("b ni ikiriting"));
+// let son;
+// if (a < b) {
+//   son = a;
+// } else if (b < a) {
+//   son = b;
+// } else {
+//   a = b;
+// }
+// console.log(son);
+
+// if 8
+// let a = parseInt(prompt("a ni ikiriting"));
+// let b = parseInt(prompt("b ni ikiriting"));
+// let max, min;
+// if (a > b) {
+//   max = a;
+//   min = b;
+//   console.log(max);
+//   console.log(min);
+// } else if (b > a) {
+//   max = b;
+//   min = a;
+//   console.log(max);
+//   console.log(min);
+// } else {
+//   console.log("xato");
+// }
+
+// IF 9
+// let a = parseInt(prompt("a ni ikiriting"));
+// let b = parseInt(prompt("b ni ikiriting"));
+// if (a > b) {
+//   let temp = a;
+//   a = b;
+//   b = temp;
+//   a + b;
+// }
+// console.log(a);
+// console.log(b);
